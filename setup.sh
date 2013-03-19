99e33b304fec2c7fc84bfcf5f96e3d29519abc11
Colin Morris <colin.morris2@gmail.com>
2013-03-19 12:53:36 -0400

    added a bunch of stuff that should have been under version control but wasn \'t

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# We don't want to keep our big data files under version control, so this script sets up soft links to them.
+# The default place to search for them is in a particular Dropbox subdir, but you may have to modify this.
+
+targets='data src/alch.db chat_logs'
+
+orig="${HOME}/Dropbox/Joel/debuggers/"
+dest="./"
+
+for t in $targets
+do
+  echo "Linking from ${orig}${t} to ${dest}${t}"
+  ln -s ${orig}${t} ${dest}${t}
+done
